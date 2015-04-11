5e02e3d6aa35ac9fa72bc324c350ec51eb751b0d
Peter van Eijk <pveijk@gmail.com>
2015-04-12 00:25:17 +0200

    another dataset

--- loadingfiles.r
+++ loadingfiles.r
@@ -3,13 +3,14 @@
 outside = read.delim("~/MyRproject/Quadfiles/weatherreport11april2015.txt", sep = ":", header = FALSE)
 dim(outside)
 sensordata <- read.csv("~/MyRproject/Quadfiles/02Jul2014.csv", header=FALSE)
+sensordata <- read.csv("~/MyRproject/Quadfiles/24may2014.csv", header=FALSE)
 # xxx  <- as.POSIXct(strptime("November 22, 2013 at 06", "%B %d, %Y"))
 # Transformation phase
 Sys.setlocale("LC_TIME",  "en_US.UTF-8")
 outside$POSIX <- as.POSIXct(strptime(outside$V1, "%B %d, %Y"))
 #outside$POSIX <- as.Date(outside$V1, "%B %d, %Y")
 
-Delta <- subset(sensordata, V1 == "Delta")
+Delta <- subset(sensordata, V1 == "Delta" & V3 >=-1 )
 # "2014-05-25 00:03:07"
 Delta$POSIX <- as.POSIXct(strptime(Delta$V2, "%Y-%m-%d %H:%M:%S"))
 
@@ -22,7 +23,7 @@ plot(outside$POSIX, outside$V4)
 plot(outside$POSIX, outside$V4, type = "l")
 hist(outside$V4)
 
-plot(Delta$POSIX, Delta$V3)
+plot(Delta$POSIX, Delta$V3, type = "l")
 mean(Delta$V3)
 daysinrange <- cut(Delta$POSIX, "day")
 x <- by(Delta, daysinrange, mean)
