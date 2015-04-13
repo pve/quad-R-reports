e2d607f6f3bf093b086d47762637e23b1bb96e6c
Peter van Eijk <pveijk@gmail.com>
2015-04-13 22:14:43 +0200

    next version. Delta and outside complete.

--- loadingfiles.r
+++ loadingfiles.r
@@ -1,4 +1,5 @@
 # comment
+Sys.setlocale("LC_TIME",  "en_US.UTF-8")
 # Extraction phase
 outside = read.delim("~/MyRproject/Quadfiles/weatherreport11april2015.txt", sep = ":", header = FALSE)
 sensordata <- read.csv("~/quad-R-reports/alldata.csv", header=FALSE)
@@ -14,19 +15,47 @@ outside$POSIX <- as.POSIXct(strptime(outside$V1, "%B %d, %Y"))
 sensordata$POSIX <- as.POSIXct(strptime(sensordata$V2, "%Y-%m-%d %H:%M:%S"))
 daysinrange <- cut(sensordata$POSIX, "day")
 range(sensordata$POSIX)
-View(daysinrange)
+#as.factor(sensordata$V1)
 
 Delta <- subset(sensordata, V1 == "Delta" & V3 >=-1 )
 Board <- subset(sensordata, V1 == "Board" & V3 >=-1 )
-View(Delta$POSIX)
-View(Board$POSIX)
+Heating <- subset(sensordata, V1 == "Heating" & V3 >=-1 )
+Return <- subset(sensordata, V1 == "Return" & V3 >=-1 )
 
-
+range(Delta$POSIX)
+range(Board$POSIX)
+range(Heating$POSIX)
+range(Return$POSIX)
+# we have more Return than Heating, more Heating than Delta measurements
 
 # Cleaning
 valid <-!is.na(outside$V4)
-outside$POSIX <- as.Date(outside$V1, "%B %d, %Y")
-outside <- subset(outside, valid)
+# names(outside$V4) <- "extT"
+
+#outside$POSIX <- as.Date(outside$V1, "%B %d, %Y")
+outside$POSIX <- as.POSIXct(strptime(outside$V1, "%B %d, %Y"))
+outside <- subset(outside[c("V4", "POSIX")], valid)
+
+
+# consolidation
+
+deltabyday <- cut(Delta$POSIX, "day") # creates factor out of date. 
+d <- aggregate(Delta[c("V3")], list(deltabyday), mean)
+d$POSIX <-as.POSIXct(d$Group.1)
+# Combination
+
+# bd <- merge(Board, Delta, by="POSIX", all = TRUE)
+# moet herhaald worden voor alle frames
+
+#od <- merge(d, outside, by = "POSIX", all = TRUE)
+od <- merge(d, outside, all = TRUE)
+plotod <- od[c("POSIX","V3", "V4")]
+
+plot(od$POSIX, od$V3, type = "s", col="red")
+points(od$POSIX, od$V4, type = "s")
+#stuck on comparison of two dates
+
+# Presentation
 
 plot(outside$POSIX, outside$V4)
 plot(outside$POSIX, outside$V4, type = "l")
@@ -34,16 +63,18 @@ hist(outside$V4)
 range(outside$POSIX)
 range(outside$V4)
 
-plot(Delta$POSIX, Delta$V3, type = "s")
-plot(Board$POSIX, Board$V3, type = "s")
+plot(Delta$POSIX, Delta$V3, type = "s", col="red")
+points(Board$POSIX, Board$V3, type = "s")
 mean(Delta$V3)
 
-daysinrange <- cut(sensordata$POSIX, "day")
 
 x <- by(Delta, daysinrange, mean)
 d <- aggregate(Delta, list(daysinrange), mean)
 plot(x$Group.1, x$V3, type = "l")
+plot(d$Group.1, d$V3, type = "l")
 
 daysinrange <- cut(Board$POSIX, "day")
 b <- aggregate(Board, list(daysinrange), mean)
+# b<- tapply(Board$V3, daysinrange, mean)
+
 plot(b$Group.1, b$V3, type = "s")
