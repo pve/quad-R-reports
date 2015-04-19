75e5b461b84d691eb6c03512bb1245b257408b07
Peter van Eijk <pveijk@gmail.com>
2015-04-19 06:45:10 +0200

    cleaning out bad sensor data, makes little difference beyond original approach

--- loadingfiles.r
+++ loadingfiles.r
@@ -1,6 +1,8 @@
 # Extracting and processing temperature sensor data.
+#more hypotheses for modelling: sun inflow and body heat give some basic delta
+#model by day of week, and light
 
-# Extraction phase; leads to dataframes
+# Ingestion phase; leads to dataframes
 Sys.setlocale("LC_TIME",  "en_US.UTF-8")
 outside = read.delim("~/MyRproject/Quadfiles/weatherreport11april2015.txt", sep = ":", header = FALSE)
 # TODO use KNMI data. 
@@ -8,61 +10,65 @@ sensordata <- read.csv("~/quad-R-reports/alldata.csv", header=FALSE)
 names(outside) <- c("V1", "V2", "reading", "extT")
 names(sensordata) <- c("source", "timestamp", "reading")
 
-# Transformation phase
+# Transformation and cleaning phase
 
+# Weather data
 # example format weatherreport as.POSIXct(strptime("November 22, 2013 at 06", "%B %d, %Y"))
 Sys.setlocale("LC_TIME",  "en_US.UTF-8")
 outside$POSIX <- as.POSIXct(strptime(outside$V1, "%B %d, %Y"))
 # we want full timestamps, so no as.Date(outside$V1, "%B %d, %Y")
-
-# sensordata date format "2014-05-25 00:03:07"
-# as.POSIXct(strptime("2014-05-25 00:03:07", "%Y-%m-%d %H:%M:%S"))
-sensordata$POSIX <- as.POSIXct(strptime(sensordata$timestamp, "%Y-%m-%d %H:%M:%S"))
-daysinrange <- cut(sensordata$POSIX, "day")
-range(sensordata$POSIX)
-
-Delta <- subset(sensordata, source == "Delta" & reading >=-2 & reading <100)
-Board <- subset(sensordata, source == "Board" & reading >=-2 & reading <100)
-Heating <- subset(sensordata, source == "Heating" & reading >=-2 & reading <100)
-Return <- subset(sensordata, source == "Return" & reading >=-2 & reading <100)
-# TODO fix systematic bias on sensors Heating and Return; Return seems to be too high.
-
-range(Delta$POSIX)
-range(Board$POSIX)
-range(Heating$POSIX)
-range(Return$POSIX)
-# we have more valid Return than Heating, more Heating than Delta measurements
-
 # Cleaning out nonsense rows.
 valid <-!is.na(outside$extT)
-
 outside$POSIX <- as.POSIXct(strptime(outside$V1, "%B %d, %Y"))
 outside <- subset(outside[c("extT", "POSIX")], valid)
 range(outside$POSIX)
 
+# Sensordata
+# sensordata date format "2014-05-25 00:03:07"
+# as.POSIXct(strptime("2014-05-25 00:03:07", "%Y-%m-%d %H:%M:%S"))
+sensordata$POSIX <- as.POSIXct(strptime(sensordata$timestamp, "%Y-%m-%d %H:%M:%S"))
+
+# faulty sensor data to get rid of
+#which days did we not trust Heating or Return?
+badsensordata <- subset(sensordata, (reading <1 | reading > 95) & (source=="Heating" | source=="Return"))
+gooddeltadata <- subset(sensordata, (reading >0 & reading <95) & (source=="Heating" | source=="Return"))
+deltabyday <- cut(badsensordata$POSIX, "DSTday") # creates factor out of date, taking care of summertime 
+badsensordays <-aggregate(badsensordata[c("reading")], list(deltabyday), length)
+#head(subset(badsensordata, (as.Date(badsensordata$POSIX) == "2014-02-15") ))
+deltabyday <- cut(gooddeltadata$POSIX, "DSTday") # creates factor out of date, taking care of summertime 
+gooddeltadays <- aggregate(gooddeltadata[c("reading")], list(deltabyday), length)
+names(gooddeltadays) <- c("POSIX", "nrreading")
+# Now we need something like 'merge' with a validsensordates
+
+Board <- subset(sensordata, source == "Board" & reading >=-2 & reading <100)
+Heating <- subset(sensordata, source == "Heating" & reading >=-2 & reading <100)
+Return <- subset(sensordata, source == "Return" & reading >=-2 & reading <100)
+     
+# TODO fix systematic bias on sensors Heating and Return; Return seems to be too high.
+
+
 # consolidation / summaries
 
-deltabyday <- cut(Delta$POSIX, "DSTday") # creates factor out of date. 
-d <- aggregate(Delta[c("reading")], list(deltabyday), mean)
-d$POSIX <-as.POSIXct(d$Group.1) # messed up by summertime. One of the dates is not 24 hours?
-# Combination
-
 # compute delta i.e. differential between influx temp and outflux temp, issue: error in one wil bias result.
-hr <-merge(Heating, Return, by = c("POSIX"))
-hr$diff <- hr$reading.x - hr$reading.y
+hr <-merge(Heating, Return, by = c("POSIX")) #ignoring points where one reading is missing
+hr$diff <- hr$reading.x - hr$reading.y -0.5
 range(hr$diff)
 #View(subset(hr, (hr$diff < -5) & (as.Date(hr$POSIX) == "2014-01-10")))
 #View(subset(hr, (as.Date(hr$POSIX) == "2014-01-10")))
+#View(subset(hr, (as.Date(hr$POSIX) == "2014-05-13")))
 # View(subset(hr, hr$diff > 60))
 
-hr <- subset(hr, hr$diff > -5 & hr$diff < 60 )
+hr <- subset(hr, hr$diff > 0 & hr$diff < 60 )
 #filter out diff < -5, > 60
 
 deltabyday <- cut(hr$POSIX, "DSTday") # creates factor out of date. 
 d <- aggregate(hr[c("diff")], list(deltabyday), mean)
 d$POSIX <-as.POSIXct(d$Group.1)
+gooddeltadays$POSIX <- as.POSIXct(gooddeltadays$POSIX)
 
-od <- merge(d, outside, by = "POSIX")
+temp <- merge(d, outside, by = "POSIX")
+od <- merge(temp, gooddeltadays, by = "POSIX")
+
 #od <- merge(hr, outside, all = TRUE)
 range(od$POSIX)
 #od$leakage <- min(0, 20 - od$extT)/od$diff
@@ -70,10 +76,11 @@ range(od$POSIX)
 od$heatloss <- od$diff/ (19 - od$extT)
 #plot(od$POSIX, min(0, 20 - od$extT)/od$diff, type = "s")
 
-plot(od$POSIX, od$heatloss, type="s")
-points(od$POSIX, od$extT, type="s")
-
 # Presentation
+
+plot(od$POSIX, od$heatloss, type="s")
+points(od$POSIX, od$extT/20, type="s", col ="red")
+
 plot(hr$POSIX, hr$diff, type = "s")
 
 plot(od$POSIX, od$diff, type = "s", col="red") # Delta
